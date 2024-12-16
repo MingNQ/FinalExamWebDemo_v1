d4877a6de9bb0397d671e48e0718b4def0981a61
Ming <minhlaminh01@gmail.com>
2024-12-16 13:44:55 +0700

    Fix bugs

--- FinalExamDemo/Models/NewShopContext.cs
+++ FinalExamDemo/Models/NewShopContext.cs
@@ -31,10 +31,6 @@ public partial class NewShopContext : DbContext
 
     public virtual DbSet<Provider> Providers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=NewShop;Integrated Security=True;Encrypt=False");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
